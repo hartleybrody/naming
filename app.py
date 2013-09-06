10e7b9ba0a2e11646f7ddc0447504df789e91fd3
Hartley Brody <hartley.brody@gmail.com>
2013-09-06 09:10:12 -0400

    added session handling and fixing bugs with prefix/suffix

--- app.py
+++ app.py
@@ -17,13 +17,14 @@ def api():
     name = str(name)
 
     prefixes = request.args.get('prefixes').split(",")
-    suffixes = request.args.get('prefixes').split(",")
+    suffixes = request.args.get('suffixes').split(",")
 
     data = []
     name_options = build_name_options(name, prefixes, suffixes)
+    http_session = requests.Session()
     for name_option in name_options:
 
-        option_data = lookup_info(name_option)
+        option_data = lookup_info(name_option, http_session)
         option_data["option"] = name_option
         data.append(option_data)
 
@@ -34,7 +35,11 @@ def api():
 
 def build_name_options(name, prefixes, suffixes):
     """takes a single name and returns common variations on it"""
-    # return [name]
+    
+    if "" not in prefixes:
+        prefixes.append("")
+    if "" not in suffixes:
+        suffixes.append("")
 
     perms = []
     for p in prefixes:
@@ -44,7 +49,7 @@ def build_name_options(name, prefixes, suffixes):
     return perms
 
 
-def lookup_info(option):
+def lookup_info(option, session):
     """takes a potential naming option and looks up info about it"""
 
     """
@@ -52,10 +57,8 @@ def lookup_info(option):
     - USPTO
     """
 
-    domainr_response = requests.get("https://domai.nr/api/json/search?q=" + option).json()
-    other_domains = []
-    com_is_available = None
-    com_register = ""
+    domainr_response = session.get("https://domai.nr/api/json/search?q=" + option).json()
+    other_domains,  com_is_available, com_register = [], None, ""
     for result in domainr_response.get("results", []):
         if result.get("path") != "":
             continue  # no one uses those path hacks...
@@ -70,17 +73,15 @@ def lookup_info(option):
                 "register": result.get("register_url")
                 })
 
-
-    return {
+    data = {
         "com_is_available": com_is_available,
         "com_register": com_register,
         "other_domains": other_domains,
-        "twitter_is_available": requests.get("http://twitter.com/" + option).status_code == 404,
-        "facebook_is_available": requests.get("http://www.facebook.com/" + option).status_code == 404,
-        # "google_results": scrape_google(option),
     }
+    data["twitter_is_available"] = session.get("http://twitter.com/" + option).status_code == 404
+    data["facebook_is_available"] = session.get("http://www.facebook.com/" + option).status_code == 404
 
-    return []
+    return data
 
 
 def scrape_google(keyword):
